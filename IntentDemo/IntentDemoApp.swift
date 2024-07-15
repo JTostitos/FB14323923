e4afd1de4521b236003acf90533a1d3cb79f2a71
JTostitos <17661536+JTostitos@users.noreply.github.com>
2024-07-15 16:26:42 -0400

    Initial Commit

---
+++ IntentDemo/IntentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntentDemoApp.swift
+//  IntentDemo
+//
+//  Created by Jonathan Tsistinas on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct IntentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
